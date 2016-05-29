847b6ca9ddeffa03d808dd1ee6a2880a1be42ccd
fgrelard <gm.florent@gmail.com>
2016-05-29 02:09:52 +0200

    fix colors

--- init.el
+++ init.el
@@ -159,3 +159,16 @@
 
 ;; Package zygospore
 (global-set-key (kbd "C-x 1") 'zygospore-toggle-delete-other-windows)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-preprocessor-face ((t (:inherit font-lock-builtin-face)))))
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
