5762f1e23dc98d4cf224c14d7ae41cf95a52a7ab
Florent <florent.grelard@u-bordeaux.fr>
2018-04-06 23:29:24 +0200

    update for JS

--- init.el
+++ init.el
@@ -67,7 +67,8 @@
     js2-refactor
     company-tern
     json-mode
-    tern-auto-complete))
+    tern-auto-complete
+    use-package))
 
 (defun install-packages ()
   "Install all required packages."
@@ -93,6 +94,8 @@
 ;; (require 'setup-ggtags)
 (require 'setup-cedet)
 ;;(require 'setup-editing)
+(require 'use-package)
+(bind-key* "C-c C-c" 'comment-or-uncomment-region)
 
 (setq-default c-basic-offset 4)
 (setq-default tab-width 4)
@@ -385,7 +388,6 @@
 ;; javascript
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(require 'company-tern)
 ;; Better imenu
 (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
 (require 'js2-refactor)
@@ -393,20 +395,15 @@
 (add-hook 'js2-mode-hook #'js2-refactor-mode)
 (js2r-add-keybindings-with-prefix "C-c C-r")
 (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
-(define-key js2-mode-map (kbd "C-c C-c") 'comment-region)
+
+
+(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'tern
+   '(progn
+      (require 'tern-auto-complete)
+      (tern-ac-setup)
+      ))
+(define-key js2-mode-map (kbd "M-RET") 'tern-ac-complete)
 
 ;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
 ;; unbind it.
-
-
-(add-to-list 'company-backends 'company-tern)
-(add-hook 'js2-mode-hook (lambda ()
-                           (tern-mode)
-                           (company-mode)))
-
-;; (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
-;; (eval-after-load 'tern
-;;    '(progn
-;;       (require 'tern-auto-complete)
-;;       (tern-ac-setup)))
-;; Disable completion keybindings, as we use xref-js2 instead
